6a2088c6bd04a88363b1c54123eaf4967e9d7663
akbarjondev <tojiyev.akbarjon@gmail.com>
2020-11-28 10:58:56 +0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// functions
+var getObject = function(selector, node = document) {
+	return document.querySelector(selector);
+}
+
+var getObjects = function(selector, node = document) {
+	return document.querySelectorAll(selector);
+}
+
+// general variables
